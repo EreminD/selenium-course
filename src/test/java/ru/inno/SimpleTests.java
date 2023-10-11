f0a968764823f91d14fcfd8d8887fe097c9d8f71
Dmitry Eremin <mitya1705@gmail.com>
2023-10-11 21:49:15 +0300

    simple tests

---
+++ src/test/java/ru/inno/SimpleTests.java
@@ -0,0 +1,508 @@
+package ru.inno;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTests {
+
+    @Test
+    public void test0() {
+        assertEquals(00, 00);
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(11, 11);
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(22, 22);
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(33, 33);
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(44, 44);
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(55, 55);
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(66, 66);
+    }
+
+    @Test
+    public void test7() {
+        assertEquals(77, 77);
+    }
+
+    @Test
+    public void test8() {
+        assertEquals(88, 88);
+    }
+
+    @Test
+    public void test9() {
+        assertEquals(99, 99);
+    }
+
+    @Test
+    public void test10() {
+        assertEquals(1010, 1010);
+    }
+
+    @Test
+    public void test11() {
+        assertEquals(1111, 1111);
+    }
+
+    @Test
+    public void test12() {
+        assertEquals(1212, 1212);
+    }
+
+    @Test
+    public void test13() {
+        assertEquals(1313, 1313);
+    }
+
+    @Test
+    public void test14() {
+        assertEquals(1414, 1414);
+    }
+
+    @Test
+    public void test15() {
+        assertEquals(1515, 1515);
+    }
+
+    @Test
+    public void test16() {
+        assertEquals(1616, 1616);
+    }
+
+    @Test
+    public void test17() {
+        assertEquals(1717, 1717);
+    }
+
+    @Test
+    public void test18() {
+        assertEquals(1818, 1818);
+    }
+
+    @Test
+    public void test19() {
+        assertEquals(1919, 1919);
+    }
+
+    @Test
+    public void test20() {
+        assertEquals(2020, 2020);
+    }
+
+    @Test
+    public void test21() {
+        assertEquals(2121, 2121);
+    }
+
+    @Test
+    public void test22() {
+        assertEquals(2222, 2222);
+    }
+
+    @Test
+    public void test23() {
+        assertEquals(2323, 2323);
+    }
+
+    @Test
+    public void test24() {
+        assertEquals(2424, 2424);
+    }
+
+    @Test
+    public void test25() {
+        assertEquals(2525, 2525);
+    }
+
+    @Test
+    public void test26() {
+        assertEquals(2626, 2626);
+    }
+
+    @Test
+    public void test27() {
+        assertEquals(2727, 2727);
+    }
+
+    @Test
+    public void test28() {
+        assertEquals(2828, 2828);
+    }
+
+    @Test
+    public void test29() {
+        assertEquals(2929, 2929);
+    }
+
+    @Test
+    public void test30() {
+        assertEquals(3030, 3030);
+    }
+
+    @Test
+    public void test31() {
+        assertEquals(3131, 3131);
+    }
+
+    @Test
+    public void test32() {
+        assertEquals(3232, 3232);
+    }
+
+    @Test
+    public void test33() {
+        assertEquals(3333, 3333);
+    }
+
+    @Test
+    public void test34() {
+        assertEquals(3434, 3434);
+    }
+
+    @Test
+    public void test35() {
+        assertEquals(3535, 3535);
+    }
+
+    @Test
+    public void test36() {
+        assertEquals(3636, 3636);
+    }
+
+    @Test
+    public void test37() {
+        assertEquals(3737, 3737);
+    }
+
+    @Test
+    public void test38() {
+        assertEquals(3838, 3838);
+    }
+
+    @Test
+    public void test39() {
+        assertEquals(3939, 3939);
+    }
+
+    @Test
+    public void test40() {
+        assertEquals(4040, 4040);
+    }
+
+    @Test
+    public void test41() {
+        assertEquals(4141, 4141);
+    }
+
+    @Test
+    public void test42() {
+        assertEquals(4242, 4242);
+    }
+
+    @Test
+    public void test43() {
+        assertEquals(4343, 4343);
+    }
+
+    @Test
+    public void test44() {
+        assertEquals(4444, 4444);
+    }
+
+    @Test
+    public void test45() {
+        assertEquals(4545, 4545);
+    }
+
+    @Test
+    public void test46() {
+        assertEquals(4646, 4646);
+    }
+
+    @Test
+    public void test47() {
+        assertEquals(4747, 4747);
+    }
+
+    @Test
+    public void test48() {
+        assertEquals(4848, 4848);
+    }
+
+    @Test
+    public void test49() {
+        assertEquals(4949, 4949);
+    }
+
+    @Test
+    public void test50() {
+        assertEquals(5050, 5050);
+    }
+
+    @Test
+    public void test51() {
+        assertEquals(5151, 5151);
+    }
+
+    @Test
+    public void test52() {
+        assertEquals(5252, 5252);
+    }
+
+    @Test
+    public void test53() {
+        assertEquals(5353, 5353);
+    }
+
+    @Test
+    public void test54() {
+        assertEquals(5454, 5454);
+    }
+
+    @Test
+    public void test55() {
+        assertEquals(5555, 5555);
+    }
+
+    @Test
+    public void test56() {
+        assertEquals(5656, 5656);
+    }
+
+    @Test
+    public void test57() {
+        assertEquals(5757, 5757);
+    }
+
+    @Test
+    public void test58() {
+        assertEquals(5858, 5858);
+    }
+
+    @Test
+    public void test59() {
+        assertEquals(5959, 5959);
+    }
+
+    @Test
+    public void test60() {
+        assertEquals(6060, 6060);
+    }
+
+    @Test
+    public void test61() {
+        assertEquals(6161, 6161);
+    }
+
+    @Test
+    public void test62() {
+        assertEquals(6262, 6262);
+    }
+
+    @Test
+    public void test63() {
+        assertEquals(6363, 6363);
+    }
+
+    @Test
+    public void test64() {
+        assertEquals(6464, 6464);
+    }
+
+    @Test
+    public void test65() {
+        assertEquals(6565, 6565);
+    }
+
+    @Test
+    public void test66() {
+        assertEquals(6666, 6666);
+    }
+
+    @Test
+    public void test67() {
+        assertEquals(6767, 6767);
+    }
+
+    @Test
+    public void test68() {
+        assertEquals(6868, 6868);
+    }
+
+    @Test
+    public void test69() {
+        assertEquals(6969, 6969);
+    }
+
+    @Test
+    public void test70() {
+        assertEquals(7070, 7070);
+    }
+
+    @Test
+    public void test71() {
+        assertEquals(7171, 7171);
+    }
+
+    @Test
+    public void test72() {
+        assertEquals(7272, 7272);
+    }
+
+    @Test
+    public void test73() {
+        assertEquals(7373, 7373);
+    }
+
+    @Test
+    public void test74() {
+        assertEquals(7474, 7474);
+    }
+
+    @Test
+    public void test75() {
+        assertEquals(7575, 7575);
+    }
+
+    @Test
+    public void test76() {
+        assertEquals(7676, 7676);
+    }
+
+    @Test
+    public void test77() {
+        assertEquals(7777, 7777);
+    }
+
+    @Test
+    public void test78() {
+        assertEquals(7878, 7878);
+    }
+
+    @Test
+    public void test79() {
+        assertEquals(7979, 7979);
+    }
+
+    @Test
+    public void test80() {
+        assertEquals(8080, 8080);
+    }
+
+    @Test
+    public void test81() {
+        assertEquals(8181, 8181);
+    }
+
+    @Test
+    public void test82() {
+        assertEquals(8282, 8282);
+    }
+
+    @Test
+    public void test83() {
+        assertEquals(8383, 8383);
+    }
+
+    @Test
+    public void test84() {
+        assertEquals(8484, 8484);
+    }
+
+    @Test
+    public void test85() {
+        assertEquals(8585, 8585);
+    }
+
+    @Test
+    public void test86() {
+        assertEquals(8686, 8686);
+    }
+
+    @Test
+    public void test87() {
+        assertEquals(8787, 8787);
+    }
+
+    @Test
+    public void test88() {
+        assertEquals(8888, 8888);
+    }
+
+    @Test
+    public void test89() {
+        assertEquals(8989, 8989);
+    }
+
+    @Test
+    public void test90() {
+        assertEquals(9090, 9090);
+    }
+
+    @Test
+    public void test91() {
+        assertEquals(9191, 9191);
+    }
+
+    @Test
+    public void test92() {
+        assertEquals(9292, 9292);
+    }
+
+    @Test
+    public void test93() {
+        assertEquals(9393, 9393);
+    }
+
+    @Test
+    public void test94() {
+        assertEquals(9494, 9494);
+    }
+
+    @Test
+    public void test95() {
+        assertEquals(9595, 9595);
+    }
+
+    @Test
+    public void test96() {
+        assertEquals(9696, 9696);
+    }
+
+    @Test
+    public void test97() {
+        assertEquals(9797, 9797);
+    }
+
+    @Test
+    public void test98() {
+        assertEquals(9898, 9898);
+    }
+
+    @Test
+    public void test99() {
+        assertEquals(9999, 9999);
+    }
+}
